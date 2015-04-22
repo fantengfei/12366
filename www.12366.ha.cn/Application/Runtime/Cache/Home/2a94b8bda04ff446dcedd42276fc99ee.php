427f7a0359526770d6c6a368646379cd9a68ba3f
fantengfei <fantengfeivip@gmail.com>
2015-04-22 21:11:40 +0800

    12366

---
+++ www.12366.ha.cn/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,18 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>www.12366.ha.cn</title>
+    <script src="/12366/www.12366.ha.cn/Public/extjs/ext-all.js"></script>
+    <script src="/12366/www.12366.ha.cn/Public/extjs/locale/ext-lang-zh_CN.js"></script>
+    <link href="/12366/www.12366.ha.cn/Public/extjs/resources/css/ext-all.css" rel="stylesheet" />
+</head>
+</head>
+<body>
+<script type="text/javascript">
+    Ext.onReady(function () {
+        Ext.MessageBox.alert("提示", "Hello world");
+    });
+</script>
+</body>
+</html>
