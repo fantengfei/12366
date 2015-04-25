1aa6ee5a7b8e623aad5c6507785d668da3e76456
fantengfei <fantengfeivip@gmail.com>
2015-04-25 17:47:05 +0800

    modify dir

---
+++ www.12366.ha.cn/Application/Home/Common/js/table.js
@@ -0,0 +1,33 @@
+$("#bodytable td").dblclick(function(){
+	addinput($(this));
+});
+
+function saveinput(obj){
+	var string = obj.val();
+	parentobj = obj.parent();
+	if(obj.parent().hasClass('tadata')){
+		if($.trim(string) == ''){
+			parentobj.html("<div class='divimage'></div><div class='divdata'>--&nbsp; &nbsp; &nbsp; &nbsp;</div>");
+		}else{
+			parentobj.html("<div class='divimage'></div><div class='divdata'>"+string+"</div>");
+		}	
+	}else{
+		parentobj.html(string);
+	}
+}
+
+function addinput(obj){
+	var inputlength = obj.children('input').length
+	if(inputlength > 0){
+		return false;
+	}
+	if(obj.hasClass('tadata')){
+		var string = obj.children('.divdata').html();
+	}else{
+		var string = obj.html();
+	}
+	var length = parseInt(obj.width())-5;
+	var color = obj.css('background-color');
+	obj.html('<input type="text" style="border:0px solid #666666;font-size:13px;font-family: 宋体;height:15px;background-color:'+color+';width:'+length+'px;"  id="editinput" value="'+string+'" onblur="saveinput($(this))" />');
+	$("#editinput").focus();
+}
--- www.12366.ha.cn/Application/Home/Conf/config.php
+++ www.12366.ha.cn/Application/Home/Conf/config.php
@@ -6,7 +6,8 @@ return array(
         '__PUBLICS__' => __ROOT__.'/Public/Home',
         '__COMMON__' => __ROOT__.'/Application/Home/Common',
         '__ICON__' => __ROOT__.'/Application/Home/Common/icon',
-        '__IMAGE__' => __ROOT__.'/Application/Home/Common/image'
+        '__IMAGE__' => __ROOT__.'/Application/Home/Common/image',
+        '__JS__' => __ROOT__.'/Application/Home/Common/js'
     )
 
 );
