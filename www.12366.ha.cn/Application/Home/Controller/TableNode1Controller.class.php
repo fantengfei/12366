1e9e2f86e53232b8336a68dea6be8225b6e6f306
fantengfei <fantengfeivip@gmail.com>
2015-04-27 15:30:10 +0800

    add table3

--- www.12366.ha.cn/Application/Home/Controller/TableNode1Controller.class.php
+++ www.12366.ha.cn/Application/Home/Controller/TableNode1Controller.class.php
@@ -27,5 +27,32 @@ class TableNode1Controller extends Controller {
         $this->display('table2');
     }
 
+    public  function  table3() {
+        $this->display('table3');
+    }
+
+    public  function  table4() {
+        $this->display('table4');
+    }
+
+    public  function  table5() {
+        $this->display('table5');
+    }
+
+    public  function  table6() {
+        $this->display('table6');
+    }
+
+    public  function  table7() {
+        $this->display('table7');
+    }
+
+    public  function  table8() {
+        $this->display('table8');
+    }
+
+    public  function  table9() {
+        $this->display('table9');
+    }
 
 }
