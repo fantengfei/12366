125502cb71515fdd424f4f3f6c4673ac2de47d05
fantengfei <fantengfeivip@gmail.com>
2015-05-11 22:18:03 +0800

    print

---
+++ www.12366.ha.cn/Application/Home/Controller/PrintController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ *
+ * User: taffy
+ * Date: 15/4/24
+ * Time: 14:23
+ */
+
+namespace Home\Controller;
+use Think\Controller;
+class PrintController extends Controller {
+
+    public function index() {
+        $this->show();
+    }
+
+}
