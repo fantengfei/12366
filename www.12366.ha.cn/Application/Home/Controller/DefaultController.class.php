2bd2b5e1618d8c99b4bf5956e5228b69d729ffdf
fantengfei <fantengfeivip@gmail.com>
2015-05-13 22:04:22 +0800

    data

--- www.12366.ha.cn/Application/Home/Controller/DefaultController.class.php
+++ www.12366.ha.cn/Application/Home/Controller/DefaultController.class.php
@@ -11,11 +11,22 @@ use Think\Controller;
 class DefaultController extends Controller {
 
     public function top() {
-        $this->show();
+        $result = M('table_info')->field('root_name')->where('table_type=3')->select();
+        $this->assign('web_name', $result[0]['root_name']);
+        $this->display('top');
     }
 
     public function home() {
         $this->show();
     }
 
+    public  function  save_name() {
+        $data['root_name']  = I('post.name');
+        $data['table_name'] = I('post.name');
+        $data['table_data'] = I('post.name');
+        $data['table_type'] = 3;
+        $data['table_url']  = I('post.name');
+        $data['id'] = 123456789;
+        M('table_info')->add($data, $options=array(), $replace=true);
+    }
 }
